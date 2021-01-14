8313d46aa4f9edd98d51639afef17aced0f190d4
Ruslan Kaidalov <rus.kaydalov@mail.ru>
2021-01-14 13:00:27 +0300

    adding snake-game and fixing bug with title size

--- project/design_on_p.py
+++ project/design_on_p.py
@@ -1,18 +1,29 @@
 import subprocess
+import os
 import sys
-from PyQt5 import uic
+from PyQt5 import uic, QtGui
 from PyQt5.QtWidgets import QMainWindow, QApplication
+
+scriptDir = os.path.dirname(os.path.realpath('data/background.png'))  # Загружаем иконку приложения
+
 
 def bird():
     subprocess.Popen(['python', 'flappy_update.py'])
+
+
+def snake():
+    subprocess.Popen(['python', 'snake_game.py'])
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('form.ui', self)  # Загружаем дизайн
+        self.setWindowTitle('КЛАССИЧЕСКИЕ ИГРЫ')
+        # self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'background.png'))
         self.setMouseTracking(True)
         self.flappy_button.clicked.connect(bird)
+        self.snake_button.clicked.connect(snake)
         self.flappy_button.setStyleSheet("background-image : url('data/background.png'); border-radius: 8px;")
 
 
